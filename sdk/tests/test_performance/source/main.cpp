03f40a632d46910e42643e1e430d7bd540140715
Fredrik Ehnbom <quarnster@gmail.com>
2012-07-23 15:33:21 +0200

    Call fflush(stdout) after printing the progress dots. Otherwise they wouldn't show up in a regular shell.

--- sdk/tests/test_performance/source/main.cpp
+++ sdk/tests/test_performance/source/main.cpp
@@ -23,7 +23,7 @@ namespace TestVector3      { void Test(double *time); }
 namespace TestAssign       { void Test(double *times); }
 
 // Times for 2.23.0
-double testTimesOrig[18] = 
+double testTimesOrig[18] =
 {1.25, // Basic
 .277,  // Basic2
 1.17,  // Call
@@ -45,7 +45,7 @@ double testTimesOrig[18] =
 };
 
 // Times for 2.23.1 WIP
-double testTimesOrig2[18] = 
+double testTimesOrig2[18] =
 {
 1.17,  // Basic
 .359,  // Basic2
@@ -84,31 +84,31 @@ int main(int argc, char **argv)
 	DetectMemoryLeaks();
 
 	printf("Performance test");
-#ifdef _DEBUG 
+#ifdef _DEBUG
 	printf(" (DEBUG)");
 #endif
 	printf("\n");
-	printf("AngelScript %s\n", asGetLibraryVersion()); 
+	printf("AngelScript %s\n", asGetLibraryVersion());
 
 	for( int n = 0; n < 18; n++ )
 		testTimesBest[n] = 1000;
 
 	for( int n = 0; n < 3; n++ )
 	{
-		TestBasic::Test(&testTimes[0]); printf(".");
-		TestBasic2::Test(&testTimes[1]); printf(".");
-		TestCall::Test(&testTimes[2]); printf(".");
-		TestCall2::Test(&testTimes[3]); printf(".");
-		TestFib::Test(&testTimes[4]); printf(".");
-		TestInt::Test(&testTimes[5]); printf(".");
-		TestIntf::Test(&testTimes[6]); printf(".");
-		TestMthd::Test(&testTimes[7]); printf(".");
-		TestString::Test(&testTimes[8]); printf(".");
-		TestString2::Test(&testTimes[9]); printf(".");
-		TestStringPooled::Test(&testTimes[10]); printf(".");
-		TestThisProp::Test(&testTimes[11]); printf(".");
-		TestVector3::Test(&testTimes[12]); printf(".");
-		TestAssign::Test(&testTimes[13]); printf(".");
+		TestBasic::Test(&testTimes[0]); printf("."); fflush(stdout);
+		TestBasic2::Test(&testTimes[1]); printf("."); fflush(stdout);
+		TestCall::Test(&testTimes[2]); printf("."); fflush(stdout);
+		TestCall2::Test(&testTimes[3]); printf("."); fflush(stdout);
+		TestFib::Test(&testTimes[4]); printf("."); fflush(stdout);
+		TestInt::Test(&testTimes[5]); printf("."); fflush(stdout);
+		TestIntf::Test(&testTimes[6]); printf("."); fflush(stdout);
+		TestMthd::Test(&testTimes[7]); printf("."); fflush(stdout);
+		TestString::Test(&testTimes[8]); printf("."); fflush(stdout);
+		TestString2::Test(&testTimes[9]); printf("."); fflush(stdout);
+		TestStringPooled::Test(&testTimes[10]); printf("."); fflush(stdout);
+		TestThisProp::Test(&testTimes[11]); printf("."); fflush(stdout);
+		TestVector3::Test(&testTimes[12]); printf("."); fflush(stdout);
+		TestAssign::Test(&testTimes[13]); printf("."); fflush(stdout);
 
 		for( int t = 0; t < 18; t++ )
 		{
@@ -118,25 +118,25 @@ int main(int argc, char **argv)
 
 		printf("\n");
 	}
-	
-	printf("Basic          %.3f    %.3f    %.3f%s\n", testTimesOrig[ 0], testTimesOrig2[ 0], testTimesBest[ 0], testTimesBest[ 0] < testTimesOrig2[ 0] ? " +" : " -"); 
-	printf("Basic2         %.3f    %.3f    %.3f%s\n", testTimesOrig[ 1], testTimesOrig2[ 1], testTimesBest[ 1], testTimesBest[ 1] < testTimesOrig2[ 1] ? " +" : " -"); 
-	printf("Call           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 2], testTimesOrig2[ 2], testTimesBest[ 2], testTimesBest[ 2] < testTimesOrig2[ 2] ? " +" : " -"); 
-	printf("Call2          %.3f    %.3f    %.3f%s\n", testTimesOrig[ 3], testTimesOrig2[ 3], testTimesBest[ 3], testTimesBest[ 3] < testTimesOrig2[ 3] ? " +" : " -"); 
-	printf("Fib            %.3f    %.3f    %.3f%s\n", testTimesOrig[ 4], testTimesOrig2[ 4], testTimesBest[ 4], testTimesBest[ 4] < testTimesOrig2[ 4] ? " +" : " -"); 
-	printf("Int            %.3f    %.3f    %.3f%s\n", testTimesOrig[ 5], testTimesOrig2[ 5], testTimesBest[ 5], testTimesBest[ 5] < testTimesOrig2[ 5] ? " +" : " -"); 
-	printf("Intf           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 6], testTimesOrig2[ 6], testTimesBest[ 6], testTimesBest[ 6] < testTimesOrig2[ 6] ? " +" : " -"); 
-	printf("Mthd           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 7], testTimesOrig2[ 7], testTimesBest[ 7], testTimesBest[ 7] < testTimesOrig2[ 7] ? " +" : " -"); 
-	printf("String         %.3f    %.3f    %.3f%s\n", testTimesOrig[ 8], testTimesOrig2[ 8], testTimesBest[ 8], testTimesBest[ 8] < testTimesOrig2[ 8] ? " +" : " -"); 
-	printf("String2        %.3f    %.3f    %.3f%s\n", testTimesOrig[ 9], testTimesOrig2[ 9], testTimesBest[ 9], testTimesBest[ 9] < testTimesOrig2[ 9] ? " +" : " -"); 
-	printf("StringPooled   %.3f    %.3f    %.3f%s\n", testTimesOrig[10], testTimesOrig2[10], testTimesBest[10], testTimesBest[10] < testTimesOrig2[10] ? " +" : " -"); 
-	printf("ThisProp       %.3f    %.3f    %.3f%s\n", testTimesOrig[11], testTimesOrig2[11], testTimesBest[11], testTimesBest[11] < testTimesOrig2[11] ? " +" : " -"); 
-	printf("Vector3        %.3f    %.3f    %.3f%s\n", testTimesOrig[12], testTimesOrig2[12], testTimesBest[12], testTimesBest[12] < testTimesOrig2[12] ? " +" : " -"); 
-	printf("Assign.1       %.3f    %.3f    %.3f%s\n", testTimesOrig[13], testTimesOrig2[13], testTimesBest[13], testTimesBest[13] < testTimesOrig2[13] ? " +" : " -"); 
-	printf("Assign.2       %.3f    %.3f    %.3f%s\n", testTimesOrig[14], testTimesOrig2[14], testTimesBest[14], testTimesBest[14] < testTimesOrig2[14] ? " +" : " -"); 
-	printf("Assign.3       %.3f    %.3f    %.3f%s\n", testTimesOrig[15], testTimesOrig2[15], testTimesBest[15], testTimesBest[15] < testTimesOrig2[15] ? " +" : " -"); 
-	printf("Assign.4       %.3f    %.3f    %.3f%s\n", testTimesOrig[16], testTimesOrig2[16], testTimesBest[16], testTimesBest[16] < testTimesOrig2[16] ? " +" : " -"); 
-	printf("Assign.5       %.3f    %.3f    %.3f%s\n", testTimesOrig[17], testTimesOrig2[17], testTimesBest[17], testTimesBest[17] < testTimesOrig2[17] ? " +" : " -"); 
+
+	printf("Basic          %.3f    %.3f    %.3f%s\n", testTimesOrig[ 0], testTimesOrig2[ 0], testTimesBest[ 0], testTimesBest[ 0] < testTimesOrig2[ 0] ? " +" : " -");
+	printf("Basic2         %.3f    %.3f    %.3f%s\n", testTimesOrig[ 1], testTimesOrig2[ 1], testTimesBest[ 1], testTimesBest[ 1] < testTimesOrig2[ 1] ? " +" : " -");
+	printf("Call           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 2], testTimesOrig2[ 2], testTimesBest[ 2], testTimesBest[ 2] < testTimesOrig2[ 2] ? " +" : " -");
+	printf("Call2          %.3f    %.3f    %.3f%s\n", testTimesOrig[ 3], testTimesOrig2[ 3], testTimesBest[ 3], testTimesBest[ 3] < testTimesOrig2[ 3] ? " +" : " -");
+	printf("Fib            %.3f    %.3f    %.3f%s\n", testTimesOrig[ 4], testTimesOrig2[ 4], testTimesBest[ 4], testTimesBest[ 4] < testTimesOrig2[ 4] ? " +" : " -");
+	printf("Int            %.3f    %.3f    %.3f%s\n", testTimesOrig[ 5], testTimesOrig2[ 5], testTimesBest[ 5], testTimesBest[ 5] < testTimesOrig2[ 5] ? " +" : " -");
+	printf("Intf           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 6], testTimesOrig2[ 6], testTimesBest[ 6], testTimesBest[ 6] < testTimesOrig2[ 6] ? " +" : " -");
+	printf("Mthd           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 7], testTimesOrig2[ 7], testTimesBest[ 7], testTimesBest[ 7] < testTimesOrig2[ 7] ? " +" : " -");
+	printf("String         %.3f    %.3f    %.3f%s\n", testTimesOrig[ 8], testTimesOrig2[ 8], testTimesBest[ 8], testTimesBest[ 8] < testTimesOrig2[ 8] ? " +" : " -");
+	printf("String2        %.3f    %.3f    %.3f%s\n", testTimesOrig[ 9], testTimesOrig2[ 9], testTimesBest[ 9], testTimesBest[ 9] < testTimesOrig2[ 9] ? " +" : " -");
+	printf("StringPooled   %.3f    %.3f    %.3f%s\n", testTimesOrig[10], testTimesOrig2[10], testTimesBest[10], testTimesBest[10] < testTimesOrig2[10] ? " +" : " -");
+	printf("ThisProp       %.3f    %.3f    %.3f%s\n", testTimesOrig[11], testTimesOrig2[11], testTimesBest[11], testTimesBest[11] < testTimesOrig2[11] ? " +" : " -");
+	printf("Vector3        %.3f    %.3f    %.3f%s\n", testTimesOrig[12], testTimesOrig2[12], testTimesBest[12], testTimesBest[12] < testTimesOrig2[12] ? " +" : " -");
+	printf("Assign.1       %.3f    %.3f    %.3f%s\n", testTimesOrig[13], testTimesOrig2[13], testTimesBest[13], testTimesBest[13] < testTimesOrig2[13] ? " +" : " -");
+	printf("Assign.2       %.3f    %.3f    %.3f%s\n", testTimesOrig[14], testTimesOrig2[14], testTimesBest[14], testTimesBest[14] < testTimesOrig2[14] ? " +" : " -");
+	printf("Assign.3       %.3f    %.3f    %.3f%s\n", testTimesOrig[15], testTimesOrig2[15], testTimesBest[15], testTimesBest[15] < testTimesOrig2[15] ? " +" : " -");
+	printf("Assign.4       %.3f    %.3f    %.3f%s\n", testTimesOrig[16], testTimesOrig2[16], testTimesBest[16], testTimesBest[16] < testTimesOrig2[16] ? " +" : " -");
+	printf("Assign.5       %.3f    %.3f    %.3f%s\n", testTimesOrig[17], testTimesOrig2[17], testTimesBest[17], testTimesBest[17] < testTimesOrig2[17] ? " +" : " -");
 
 	printf("--------------------------------------------\n");
 	printf("Press any key to quit.\n");
